0ef4571ab916d04809f125578528468fc4d6e73e
Tim Bavaro <tbavaro@gmail.com>
2019-04-28 08:19:13 -0700

    basic webmidi hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,81 @@
 import * as React from "react";
 import "./App.css";
 
-import logo from "./logo.svg";
+type MidiState = {
+  status: "initializing"
+} | {
+  status: "loaded",
+  webMidi: WebMidi.MIDIAccess
+} | {
+  status: "failed",
+  midiFailureReason: any
+};
 
-class App extends React.Component {
+interface State {
+  midiState: Readonly<MidiState>;
+};
+
+class App extends React.Component<{}, State> {
+  public state: State = {
+    midiState: {
+      status: "initializing"
+    }
+  };
+
+  public componentWillMount() {
+    if (super.componentWillMount) {
+      super.componentWillMount();
+    }
+
+    navigator.requestMIDIAccess().then(webMidi => {
+      this.setState({
+        midiState: {
+          status: "loaded",
+          webMidi: webMidi
+        }
+      });
+    }).catch(reason => {
+      console.log("exception requesting MIDI access", reason);
+      this.setState({
+        midiState: {
+          status: "failed",
+          midiFailureReason: reason
+        }
+      });
+    });
+  }
+
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className="App" children={this.renderBody()} />
+    );
+  }
+
+  private renderBody() {
+    switch (this.state.midiState.status) {
+      case "initializing":
+        return "Initializing...";
+
+      case "loaded":
+        return this.renderLoadedBody(this.state.midiState.webMidi);
+
+      case "failed":
+        return "failed";
+    }
+  }
+
+  private renderLoadedBody(webMidi: WebMidi.MIDIAccess) {
+    const names = Array.from(webMidi.outputs.keys());
+    console.log("names", names);
+
+    return (
+      <div className="App-devices">
+        <div>Devices ({names.length}):</div>
+        <ul>
+          {
+            names.map((name, i) => <li key={i}>{name}</li>)
+          }
+        </ul>
       </div>
     );
   }
