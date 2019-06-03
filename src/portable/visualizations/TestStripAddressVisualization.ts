d27a7ce61f21ef68a5f19e1fd0038f822163ff5d
Tim Bavaro <tbavaro@gmail.com>
2019-06-02 22:40:20 -0700

    make strip address visualization show hardward addresses

--- src/portable/visualizations/TestStripAddressVisualization.ts
+++ src/portable/visualizations/TestStripAddressVisualization.ts
@@ -2,31 +2,39 @@ import * as Colors from "../base/Colors";
 import LedInfo from "../base/LedInfo";
 import * as PianoVisualization from "../base/PianoVisualization";
 
-const MINOR_SEGMENT_LENGTH = 64;  // fadecandy channel
-const MAJOR_SEGMENT_LENGTH = 512;  // fadecandy unit
-
-const MAJOR_SEGMENT_START_COLOR = Colors.WHITE;
-const MINOR_SEGMENT_START_BRIGHTNESS = 1;
-const NORMAL_BRIGHTNESS = 0.3;
-const PULSE_BRIGHTNESS = 0.5;
+const CHANNEL_START_COLOR = Colors.WHITE;
+const NORMAL_BRIGHTNESS = 0.5;
+const PULSE_BRIGHTNESS = 1.0;
 
 const LED_ZERO_BLINK_TIME = 1000;
 
 const PULSE_SEPARATION_LEDS = 8;
 const PULSE_SPEED_LEDS_PER_SECOND = 16;
 
-const SEGMENT_HUE_INCREMENT = 210;
-
 export default class TestStripAddressVisualization extends PianoVisualization.default {
   private timeCounter: number = 0;
   private pulseLocationFloat: number = 0;
+  private readonly ledInfos: LedInfo[][];
+  private readonly channelHueIncrement: number;
 
   constructor(ledInfos: LedInfo[][]) {
     super(ledInfos.map(row => row.length));
+    this.ledInfos = ledInfos;
+
+    let maxChannel = 0;
+    ledInfos.forEach(row => row.forEach(led => {
+      if (led.hardwareChannel > maxChannel) {
+        maxChannel = led.hardwareChannel;
+      }
+    }));
+
+    const numChannels = maxChannel + 1;
+
+    this.channelHueIncrement = 360 / numChannels;
   }
 
   public render(elapsedMillis: number): void {
-    this.pulseLocationFloat = (this.pulseLocationFloat + PULSE_SPEED_LEDS_PER_SECOND * elapsedMillis / 1000) % MINOR_SEGMENT_LENGTH;
+    this.pulseLocationFloat = (this.pulseLocationFloat + PULSE_SPEED_LEDS_PER_SECOND * elapsedMillis / 1000) % PULSE_SEPARATION_LEDS;
     const pulseLocation = Math.floor(this.pulseLocationFloat);
 
     this.timeCounter = (this.timeCounter + elapsedMillis) % LED_ZERO_BLINK_TIME;
@@ -39,18 +47,17 @@ export default class TestStripAddressVisualization extends PianoVisualization.de
     });
   }
 
-  private colorForPixel(rowIdx: number, n: number, flashBrightness: number, pulseLocation: number): Colors.Color {
-    if (n % MAJOR_SEGMENT_LENGTH <= rowIdx) {
-      return Colors.multiply(MAJOR_SEGMENT_START_COLOR, flashBrightness);
-    } else if (n % MAJOR_SEGMENT_LENGTH <= rowIdx) {
-      return MAJOR_SEGMENT_START_COLOR;
+  private colorForPixel(rowNumber: number, rowIndex: number, flashBrightness: number, pulseLocation: number): Colors.Color {
+    const ledInfo = this.ledInfos[rowNumber][rowIndex];
+    const n = ledInfo.hardwareIndex;
+
+    if (n > 0 && n <= (ledInfo.hardwareChannel + 1)) {
+      return Colors.multiply(CHANNEL_START_COLOR, flashBrightness);
     } else {
-      const segmentNumber = Math.floor(n / MINOR_SEGMENT_LENGTH);
-      const hue = (segmentNumber * SEGMENT_HUE_INCREMENT) % 360;
+      const segmentNumber = ledInfo.hardwareChannel;
+      const hue = (segmentNumber * this.channelHueIncrement) % 360;
       let brightness;
-      if (n % MINOR_SEGMENT_LENGTH <= rowIdx) {
-        brightness = MINOR_SEGMENT_START_BRIGHTNESS;
-      } else if ((n - pulseLocation) % PULSE_SEPARATION_LEDS === 0) {
+      if ((n - pulseLocation) % PULSE_SEPARATION_LEDS === 0) {
         brightness = PULSE_BRIGHTNESS;
       } else {
         brightness = NORMAL_BRIGHTNESS;
