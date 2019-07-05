23389aa027140c3b2edab5af5ebc44350c2849f9
Andrew Colombi <acolombi@gmail.com>
2019-07-05 11:32:34 -0700

    improvements

--- src/portable/visualizations/ExpandingDashesVisualization.ts
+++ src/portable/visualizations/ExpandingDashesVisualization.ts
@@ -1,14 +1,19 @@
-import { bracket01 } from "../../util/Utils";
+import { bracket01, valueOrDefault } from "../../util/Utils";
+import ColorRow from "../base/ColorRow";
 import * as Colors from "../base/Colors";
+import FancyValue from "../base/FancyValue";
 import * as Visualization from "../base/Visualization";
+import BasicAudioHelper from "./util/BasicAudioHelper";
 
 const NAME = "expandingDashes";
 
 class ExpandingDashesVisualization extends Visualization.default {
 
     private readonly palette: Colors.Color[];
-    private readonly wingDashes: number[];
+    private readonly wingDashPaires: number[];
+    private readonly wingDashPairRatioes: number[];
     private readonly ezTimeseries: Visualization.EasyTimeSeriesValueSetters;
+    private readonly helper: MultiLevelHelper;
 
     constructor(config: Visualization.Config) {
         super(config);
@@ -16,39 +21,130 @@ class ExpandingDashesVisualization extends Visualization.default {
         this.palette = hexPalette.map(Colors.hex2Color, hexPalette);
         shuffleArray(this.palette);
 
-        this.wingDashes = [0, 0, 0, 0].map(_ => Math.round(Math.random() * 5) + 5);
+        this.wingDashPaires = [0, 0, 0, 0].map(_ => Math.round(Math.random() * 3) + 3);
+        this.wingDashPairRatioes = [0, 0, 0, 0].map(_ => Math.random() * 0.5 + 0.25);
 
         this.ezTimeseries = config.createEasyTimeSeriesSet();
+
+        this.helper = new MultiLevelHelper(config.audioSource);
     }
 
     public render(context: Visualization.FrameContext): void {
+        this.helper.sample(context.elapsedMillis);
+
         this.ledRows.forEach(r => r.fill(Colors.BLACK));
+
+        this.ezTimeseries.orange.value = this.helper.lowLevel;
+        this.ezTimeseries.white.value = this.helper.highLevel;
 
         this.ledRows.forEach((row, rowIdx) => {
             const wingRowLength = row.length / 2;
-            const wingRowDashes = this.wingDashes[rowIdx];
-            const ledsPerDash = Math.floor(wingRowLength / wingRowDashes);
-            const rowOffset = Math.floor((wingRowLength - ledsPerDash * wingRowDashes) / 2);
-            const radius = Math.sin(Date.now() / 500) * 0.5 + 0.5;
-            this.ezTimeseries.orange.value = radius;
-            for (let i = 1; i <= wingRowDashes; ++i) {
-                const center = Math.round(rowOffset + i * ledsPerDash - ledsPerDash/2);
-                const radiusInLeds = radius * ledsPerDash/2;
-                const halfLedsToLight = Math.ceil(radiusInLeds);
-                for (let l = center - halfLedsToLight; l <= center + halfLedsToLight; ++l) {
-                    const aliasing = bracket01(radiusInLeds - Math.abs(l - center));
-                    row.add(l, Colors.multiply(this.palette[rowIdx], aliasing));
-                    row.add(row.length - l, Colors.multiply(this.palette[rowIdx], aliasing));
-                }
+            const wingRowDashPaires = this.wingDashPaires[rowIdx];
+            const ledsPerDashPair = Math.floor(wingRowLength / wingRowDashPaires);
+            const rowOffset = Math.floor((wingRowLength - ledsPerDashPair * wingRowDashPaires) / 2);
+            for (let i = 0; i < wingRowDashPaires; ++i) {
+                const dashPairStart = Math.round(rowOffset + i * ledsPerDashPair);
+                const firstDashStart = dashPairStart;
+                const ledsInFirstDash = Math.round(ledsPerDashPair * this.wingDashPairRatioes[rowIdx]);
+                const firstDashLevel = stupid(rowIdx % 2 === 0 ? this.helper.lowLevel : this.helper.highLevel);
+                this.renderDash(firstDashStart, ledsInFirstDash, firstDashLevel, rowIdx+i, row);
+
+                const secondDashStart = firstDashStart + ledsInFirstDash;
+                const ledsInSecondDash = ledsPerDashPair - ledsInFirstDash;
+                const secondDashLevel = stupid(rowIdx % 2 === 1 ? this.helper.lowLevel : this.helper.highLevel);
+                this.renderDash(secondDashStart, ledsInSecondDash, secondDashLevel, rowIdx+i*7, row);
             }
         });
     }
+
+    private renderDash(dashStart: number, ledsInDash: number, radius: number, dashKey: number, row: ColorRow) {
+        const center = dashStart + ledsInDash / 2;
+        const dashLo = Math.floor(center - ledsInDash / 2);
+        const dashHi = Math.ceil(center + ledsInDash / 2);
+        const radiusInLeds = radius * ledsInDash;
+        for (let l = dashLo; l <= dashHi; ++l) {
+            const aliasing = bracket01(radiusInLeds - Math.abs(l - center));
+            row.add(l, Colors.multiply(this.randomColor(dashKey), aliasing));
+            row.add(row.length - l, Colors.multiply(this.randomColor(dashKey *7), aliasing));
+        }
+    }
+
+    private randomColor(key: number) {
+        return this.palette[key % this.palette.length];
+    }
+}
+
+function stupid(x: number) {
+    return ((x * 3) * 0.9 + 0.1) * 0.9;
 }
 
 function shuffleArray(array: any[]) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+class LevelsHelper {
+    private readonly v: FancyValue = new FancyValue();
+    private readonly halfLife: number;
+    private readonly minThreshold: number;
+    private readonly maxThreshold: number;
+
+    constructor(attrs: {
+        halfLife: number,
+        minThreshold?: number,
+        maxThreshold?: number
+    }) {
+        this.halfLife = attrs.halfLife;
+        this.minThreshold = valueOrDefault(attrs.minThreshold, 0);
+        this.maxThreshold = valueOrDefault(attrs.maxThreshold, 1);
+    }
+
+    public processValue(newValue: number, elapsedMillis: number) {
+        const value = bracket01((newValue - this.minThreshold) / (this.maxThreshold - this.minThreshold));
+        this.v.decayExponential(this.halfLife, elapsedMillis / 1000);
+        this.v.bumpTo(value);
+    }
+
+    public get value() {
+        return this.v.value;
+    }
+}
+
+class MultiLevelHelper {
+    private readonly audioHelper: BasicAudioHelper;
+    private readonly lowHelper: LevelsHelper;
+    private readonly highHelper: LevelsHelper;
+
+    constructor(audioSource: AudioNode | null) {
+        this.audioHelper = new BasicAudioHelper(audioSource);
+
+        this.lowHelper = new LevelsHelper({
+            halfLife: 0.125,
+            minThreshold: 0.225,
+            maxThreshold: 0.6
+        });
+
+        this.highHelper = new LevelsHelper({
+            halfLife: 0.125,
+            minThreshold: 0.1,
+            maxThreshold: 0.75
+        });
+    }
+
+    public sample(elapsedMillis: number) {
+        const audioValues = this.audioHelper.getValues();
+        this.lowHelper.processValue(audioValues.lowRMS, elapsedMillis);
+        this.highHelper.processValue(audioValues.highRMS, elapsedMillis);
+    }
+
+    public get lowLevel() {
+        return this.lowHelper.value;
+    }
+
+    public get highLevel() {
+        return this.highHelper.value;
     }
 }
 
