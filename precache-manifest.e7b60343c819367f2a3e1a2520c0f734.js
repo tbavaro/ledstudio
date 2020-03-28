d9059b5a1730e02ae6eaa6257145cd0284cc6842
Tim Bavaro <tbavaro@gmail.com>
2020-03-28 12:04:34 -0700

    deploy v0.23.0

---
+++ precache-manifest.e7b60343c819367f2a3e1a2520c0f734.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd68cce70b2b38e6a990",
+    "url": "./static/css/main.2111608b.chunk.css"
+  },
+  {
+    "revision": "dd68cce70b2b38e6a990",
+    "url": "./static/js/main.dd68cce7.chunk.js"
+  },
+  {
+    "revision": "679da5760b1a3d93a3c9",
+    "url": "./static/js/1.679da576.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "1d0d8aaa2aa7810098faf90b27a8f70b",
+    "url": "./index.html"
+  }
+];
